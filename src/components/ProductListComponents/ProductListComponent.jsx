9f61418a4ab1da230cb7703b9b5522ea55292ed8
mekanicas <brunomicelio52@gmail.com>
2024-06-03 23:58:26 -0300

    adding products and data

--- src/components/ProductListComponents/ProductListComponent.jsx
+++ src/components/ProductListComponents/ProductListComponent.jsx
@@ -1,32 +1,81 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
 import { getAllProducts } from "../../services/productsServices";
 
 const ProductsListComponent = () => {
   const [products, setProducts] = React.useState([]);
+  const customItemStyle = {
+    backgroundColor: "#23262C",
+    color: "white",
+    padding: "10px",
+    height: "600px",
+    flexDirection: "column",
+  };
+  const customImageStyle = {
+    objectFit: "contain",
+    maxHeight: "200px",
+    width: "100%",
+    marginBottom: "10px",
+  };
+  const customItemText = {
+    overflowY: "hidden",
+    textOverFlow: "ellipsis",
+    lineHeight: "1.5",
+    height: "50px",
+  };
+  const customGroupStyle = {
+    backgroundColor: "#2A2D33",
+    color: "white",
+    border: "none",
+  };
+
   React.useEffect(() => {
-    getAllProducts()
-      .then((res) => setProducts(res.data.products))
-      .catch((err) => console.error(err));
+    const fetchProducts = async () => {
+      try {
+        const fetchedProducts = await getAllProducts();
+        setProducts(fetchedProducts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
   }, []);
   return (
-    <Row xs={1} md={2} className="g-4">
-      <Col>
-        {products.map((product) => (
-          <Card key={product.id} className="mt-2" style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={product.thumbnail} />
+    <Row xs={1} md={3} className="g-4">
+      {" "}
+      {products.map((product, index) => (
+        <Col key={product.id}>
+          {" "}
+          <Card className="mt-2" style={customItemStyle}>
+            <Card.Img
+              style={customImageStyle}
+              variant="top"
+              src={product.image}
+            />
             <Card.Body>
               <Card.Title>{product.title}</Card.Title>
-              <Card.Text>{product.description}</Card.Text>
+              <Card.Text style={customItemText}>
+                {product.description}
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item style={customGroupStyle}>
+                Categoría : {product.category}
+              </ListGroup.Item>
+              <ListGroup.Item className="fs-1" style={customGroupStyle}>
+                $ {product.price}
+              </ListGroup.Item>
+            </ListGroup>
+            <Card.Body className="mt-2">
               <Button variant="primary">Ir a detalle</Button>
             </Card.Body>
           </Card>
-        ))}
-      </Col>
+        </Col>
+      ))}
     </Row>
   );
 };
--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -1,4 +1,10 @@
 import axios from "axios";
-export const getAllProducts = () => {
-  return axios.get("https://dummyjson.com/products");
+export const getAllProducts = async () => {
+  try{
+    const response = await axios.get('https://fakestoreapi.com/products')
+    return response.data;
+  } catch(error){
+    console.error('Error fetching products : ', error)
+  }
+  return[];
 };
